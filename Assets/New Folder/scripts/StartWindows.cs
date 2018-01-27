e991c3abfd26b013d54eba06da45e37009eaae91
薛宇涛 <xueyutao@xueyutaodeMacBook-Air.local>
2018-01-27 23:10:07 +0800

    合并

---
+++ Assets/New Folder/scripts/StartWindows.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartWindows : MonoBehaviour 
+{
+	[SerializeField] private bool flag;
+	[SerializeField] private Image image;
+
+	#region Unity Methods
+
+	void Awake () 
+	{
+		
+	}
+
+	void Start () 
+	{
+		flag = false;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown ("space"))
+		{
+			EnterGame ();
+		}
+
+		if (flag)
+		{
+			Color init, end;
+			init = image.color;
+			end = Color.black;
+			image.color = Color.Lerp (init, end, Time.deltaTime);
+		}
+	}
+
+	#endregion
+
+	private void EnterGame ()
+	{
+		flag = true;
+		Invoke ("loadScene", 2f);
+	}
+
+	void loadScene()
+	{
+		SceneManager.LoadScene ("jiaocheng");
+	}
+}
---
+++ Assets/New Folder/scripts/Wheel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void WMovement(float speed){
+		transform.Rotate (0, 0, -speed*250 * Time.deltaTime);
+	}
+}
