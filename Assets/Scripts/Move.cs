4b86f863e0304498cd27d1a8537158897a51587f
薛宇涛 <xueyutao@xueyutaodeMacBook-Air.local>
2018-01-27 20:18:07 +0800

    flow 2

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,7 +6,7 @@ public class Move : MonoBehaviour
 {
 	public static Move instance;
 
-	public float timer = 10;
+	public float timer = 10; //初始
 	public string status;
 	public bool active;
 
@@ -25,8 +25,8 @@ public class Move : MonoBehaviour
 		if (active) 
 		{
 			timer -= MoveManager.instance.speed;
-			transform.position = new Vector3 (timer, 0, 2);
-			if (timer < -20f) 
+			transform.position = new Vector3 (-timer, 0, 2);
+			if (timer < -15f) 
 			{
 				Destroy (this.gameObject);
 			}
--- Assets/Scripts/MoveManager.cs
+++ Assets/Scripts/MoveManager.cs
@@ -7,44 +7,61 @@ public class MoveManager : MonoBehaviour
 {
 	public static MoveManager instance;
 
-	public Vector3 spawnPos, endPos;
-	public Text m_text; //打印
-	public GameObject prefab; //预制体
-	public Transform hand; //预制体prefab
-	public RectTransform milk; //预制体prefab
 	public float speed; //控制全局速度
 	public float timespan; //控制发射间隔
-	float timer = 0;
-	float holdTime = 0;
+	[SerializeField] private Vector3 spawnPos, endPos;
+	[SerializeField] private Text m_debugText; //打印
+	[SerializeField] private Text m_scoreText; //计分板
+	[SerializeField] private float timer = 0;
+	[SerializeField] private GameObject scoreBoard;
+	[SerializeField] private GameObject prefab; //预制体
+	[SerializeField] private Transform hand;
+	[SerializeField] private RectTransform milk; //桶mask
+	[SerializeField] private Transform target; //当前目标
+	private float holdTime = 0;
 	public Toggle going;
-	public Transform target; //当前目标体
+	[SerializeField] private int cowMax = 10; //牛的总量，cowCount等于cowMax时停止Spawn，结算
+	/*[SerializeField]*/ private int cowCount = 0; //牛的数量
+	/*[SerializeField]*/ private int bucketSuccess = 0; //伸桶成功的数量
+	/*[SerializeField]*/ private int milkingSuccess = 0; //挤奶成功的数量
 
 	void Awake () 
 	{
 		instance = this;
+		going.onValueChanged.AddListener ((bool value) => OnStartGame(value));
 	}
 
 	void Start()
 	{
-		//Spawn ();
+		scoreBoard.SetActive (false);
 	}
 
 	void Update ()
 	{
-		//
+		//计分
+		Score ();
+
+		//获取状态
 		timer += Time.deltaTime;
 		if (timer >= timespan && going.isOn)
 		{
-			Spawn ();
-			timer = 0;
+			if (cowCount > cowMax) 
+			{
+				PushScoreBoard ();
+			} 
+			else
+			{
+				Spawn ();
+				timer = 0;
+			}
 		}
 
+		//操作
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
-			m_text.text = "";
+			m_debugText.text = "";
 			holdTime = 0;
 		}
-
 		if (Input.GetKey (KeyCode.Space)) 
 		{
 			//手的移动
@@ -60,14 +77,16 @@ public class MoveManager : MonoBehaviour
 			{
 				switch (hitInfo.transform.name) 
 				{
-				case "prefab":
+				case "cow1":
 					if (string.IsNullOrEmpty (hitInfo.transform.GetComponent<Move> ().status)) 
 					{
 						hitInfo.transform.GetComponent<Move> ().status = "good";
 						hitInfo.transform.GetComponent<Move> ().Stop ();
 						target = hitInfo.transform;
-						Debug.Log ("good");
+						Debug.Log ("good"); //伸桶成功 bucketSuccess
+						bucketSuccess += 1;
 					}
+
 					if(hitInfo.transform.GetComponent<Move> ().status == "good")
 					{
 						holdTime += Time.deltaTime;
@@ -75,16 +94,17 @@ public class MoveManager : MonoBehaviour
 						{
 							milk.localPosition += new Vector3 (0, 0.5f, 0); //0.01f 根据sprite大小，装载时间更改
 						}
-						m_text.text = "good " + holdTime.ToString("f1");
+						m_debugText.text = "good " + holdTime.ToString("f1");
 						going.isOn = false;
 						if (holdTime > 4) 
 						{
 							//hold太久了，按过头
 							Debug.Log("too much");
-							m_text.text = "too much";
+							m_debugText.text = "too much";
 							hitInfo.transform.GetComponent<Move> ().Continue();
 							going.isOn = true;
 							target = null;
+							//timer = 0;
 						}
 					}
 					break;
@@ -93,7 +113,7 @@ public class MoveManager : MonoBehaviour
 					{
 						hitInfo.transform.GetComponentInParent<Move> ().status = "bad";
 						Debug.Log ("bad");
-						m_text.text = "bad";
+						m_debugText.text = "bad";
 					}
 					break;
 				}
@@ -101,14 +121,13 @@ public class MoveManager : MonoBehaviour
 			else //按下键时，未检测到物体
 			{
 				//if(当前target还没有状态)
-				if (m_text.text == "miss" || string.IsNullOrEmpty (m_text.text)) 
+				if (m_debugText.text == "miss" || string.IsNullOrEmpty (m_debugText.text)) 
 				{
-					m_text.text = "miss";
+					m_debugText.text = "miss";
 				}
 				//如果已经赋有状态，不变
 			}
 		}
-
 		if (Input.GetKeyUp (KeyCode.Space))
 		{
 			milk.localPosition = new Vector3 (0, -100f, 0);
@@ -123,38 +142,64 @@ public class MoveManager : MonoBehaviour
 				if (holdTime < 3)
 				{
 					//时间不够
-					m_text.text = "not enough";
+					m_debugText.text = "not enough";
 					Debug.Log("not enough");
 				} 
 				else if (holdTime > 4) //不会在这里执行too much
 				{
 					//hold太久了
 					Debug.Log("too much");
-					m_text.text = "too much";
+					m_debugText.text = "too much";
 				}
 				else 
 				{
 					//正好
-					m_text.text = "正好";
-					Debug.Log("正好");
+					m_debugText.text = "good";
+					Debug.Log("good");
+					milkingSuccess += 1; //挤奶成功
 				}
 
 				going.isOn = true;
 				target = null;
+				timer = 0;
 			}
 
-			if (m_text.text == "miss") 
+			if (m_debugText.text == "miss")
 			{
-				//m_text.text = "";
+				//m_debugText.text = "";
 			}
 		}
 	}
 
-	[ContextMenu("Spawn")]
+	//刷下一个牛
 	void Spawn()
 	{
 		GameObject go = Instantiate (prefab);
 		go.transform.position = spawnPos;
-		go.name = "prefab";
+		go.name = "cow1"; //随机
+		cowCount += 1;
+	}
+
+	//计分函数
+	void Score()
+	{
+		m_scoreText.text = "牛数量: " + cowCount
+			+ "\n伸桶成功: " + bucketSuccess + " 成功率: " + ((cowCount == 0)? 0 : (float)bucketSuccess *100 / (float)cowCount).ToString("f0") + "%"
+			+ "\n挤奶成功: " + milkingSuccess + " 成功率: " + ((cowCount == 0)? 0 : (float)milkingSuccess * 100 / (float)cowCount).ToString("f0") + "%";
+	}
+
+	//游戏开始开关
+	void OnStartGame(bool active)
+	{
+		if (active) 
+		{
+			timer = 0;
+		}
+	}
+
+	//弹出计分板
+	void PushScoreBoard()
+	{
+		scoreBoard.SetActive (true);
 	}
 }
